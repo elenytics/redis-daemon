7068601651ffa40f64315f7e9c78e8e375f98367
Sayan Ghosh <sggsayan93@gmail.com>
2016-07-19 15:21:18 -0400

    Edited calibration.py

--- calibration.py
+++ calibration.py
@@ -40,9 +40,40 @@ def get_messages(x, y, floor):
     except KeyboardInterrupt:
        return True 
 
-while True:
-    x = input("Enter x: ")
-    y = input("Enter y: ")
-    floor = input("Enter floor: ")
-    get_messages(x, y, floor)
+try: 
+    while True:
+        x = input("Enter x: ")
+        y = input("Enter y: ")
+        floor = input("Enter floor: ")
+        get_messages(x, y, floor)
+except KeyboardInterrupt:
+    with open('inputs.out', 'r') as inputs:
+        lines = [line.rstrip('\n') for line in open('inputs.out')]
 
+    data = []
+    for line in lines:
+        data.append(line.split('  '))
+
+    pairs = []
+    for line in data:
+        for pair in line:
+            pairs.append(pair.split(','))
+
+    for pair in pairs:
+        if len(pair) > 2:
+            del pair[-1]
+
+    pairs = [pair for pair in pairs if pair != ['']]
+
+    aps = {}
+
+    for pair in pairs:
+        if pair[0] not in aps:
+            aps[pair[0]] = [pair[1]]
+        else:
+            aps[pair[0]].append(pair[1])
+
+    data = open('data.out', 'a')
+    for ap in aps:
+        open.write(ap)
+        open.write(', '.join(map(str, aps[ap]))
