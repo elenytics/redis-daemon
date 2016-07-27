47340f243eaa462ea2d0deada92375f43a4cb19e
Elenytics <ubuntu@ip-172-31-2-246.us-west-1.compute.internal>
2016-07-27 04:42:29 +0000

    Completed communication between server 1 and 2

--- redis-daemon.py
+++ redis-daemon.py
@@ -11,21 +11,23 @@ r2 = redis.StrictRedis(host='54.149.167.108', password='elenyticsneural', port=6
 def my_handler(message):
     with open('data.out', 'a') as file: 
         data = message['data'].split(';')
-        file.write(message)
+        file.write('%s' % ' '.join(map(str, data)))
+        file.write('\n')
         device = message['channel']
-        p2.publish(device, data)
+        r2.publish(device, ' '.join(map(str, data)))
+    return True
     
 
 p = r.pubsub(ignore_subscribe_messages=True)
 p.psubscribe(**{'*': my_handler})
 
 p2 = r2.pubsub(ignore_subscribe_messages=True)
+p2.psubscribe(**{'*': my_handler})
 
 class RedisDaemon(Daemon):
     def run(self):
         while True:
 	    p.get_message()
-	    time.sleep(0.005)
 
 if __name__ == "__main__":
         daemon = RedisDaemon('/tmp/redis-daemon.pid')
